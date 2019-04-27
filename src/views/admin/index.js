c64cfd8bb7662ad4cf533c5d3a625ac7d278f97b
Ivan Gonzalez <ivandario1998@gmail.com>
2019-04-27 09:55:05 -0500

    Upload title and description

--- src/views/admin/index.js
+++ src/views/admin/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import * as firebase from "firebase";
-import TextField from "@material-ui/core/TextField";
+import Input from "@material-ui/core/Input";
 import { Container, Form, Button, Card } from "react-bootstrap";
 
 class Admin extends Component {
@@ -8,35 +8,34 @@ class Admin extends Component {
     super(props);
 
     this.state = {
-      problems: [],
-      show: false
+      name1: "",
+      name2: "",
+      name3: "",
+      name4: "",
+      name5: "",
+      description1: "",
+      description2: "",
+      description3: "",
+      description4: "",
+      description5: ""
     };
-
-    this.problems = [];
-
-    this.amount = [0, 1, 2, 3, 4];
-    this.properties = ["description", "name"];
   }
 
   async componentDidMount() {
-    var tempArray = [];
-    for (let i = 0; i < this.amount.length; i++) {
-      var tempObject = {};
-      for (let j = 0; j < this.properties.length; j++) {
-        tempObject[this.properties[j]] = "";
-      }
-      tempArray.push(tempObject);
-    }
-    await this.setState({ problems: tempArray });
-    this.setState({ show: true });
-    /*var database = firebase.database().ref();
+    var database = firebase.database().ref();
     database
       .child("problems")
       .once("value")
       .then(data => {
-        this.setState({ problems: data.val() });
-        console.log(this.state.problems);
-      });*/
+        const dbinfo = data.toJSON();
+        console.log(data.toJSON());
+        for (let index = 1; index < 6; index++) {
+          this.setState({
+            ["name" + index]: dbinfo["name" + index],
+            ["description" + index]: dbinfo["description" + index]
+          });
+        }
+      });
   }
 
   enviar = () => {
@@ -44,23 +43,15 @@ class Admin extends Component {
   };
 
   update(e) {
-    var database = firebase.database().ref();
     e.preventDefault();
-    for (let index = 0; index < this.amount.length; index++) {
-      var tempObject = {};
-      if (this.state["name" + index]) {
-        tempObject["name"] = this.state["name" + index];
-      }
-      if (this.state["description" + index]) {
-        tempObject["description"] = this.state["description" + index];
-      }
-      this.problems[index] = tempObject;
+    var database = firebase.database().ref();
+    var tempObject = {};
+    for (let index = 1; index < 6; index++) {
+      tempObject["name" + index] = this.state["name" + index];
+      tempObject["description" + index] = this.state["description" + index];
     }
-    console.log(this.problems);
-    this.setState({
-      problems: this.problems
-    });
-    database.child("problems").update(this.problems);
+    database.child("problems").set(tempObject);
+    window.location.reload();
   }
 
   handleChange(e) {
@@ -76,44 +67,43 @@ class Admin extends Component {
           <h1>Actualiza las problematicas</h1>
         </center>
         <br />
-        {this.state.show && (
-          <Form onSubmit={e => this.update(e)}>
-            {this.amount.map(index => {
-              return (
-                <div key={index}>
-                  <Card.Title>Problemático #{index}</Card.Title>
-                  <Form.Group>
-                    <Form.Label>Nombre</Form.Label>
-                    <Form.Control
-                      id={"name" + index}
-                      defaultValue={this.state.problems[index]["name"]}
-                      placeholder="Nombre de la problemática"
-                      onChange={e => this.handleChange(e)}
-                    />
-                  </Form.Group>
-                  <Form.Group>
-                    <Form.Label>Descripción</Form.Label>
-                    <Form.Control
-                      as="textarea"
-                      rows="4"
-                      id={"description" + index}
-                      defaultValue={this.state.problems[index]["description"]}
-                      placeholder="Una pequeña descripción de la problemática"
-                      onChange={e => this.handleChange(e)}
-                    />
-                  </Form.Group>
-                  {index !== this.amount.length - 1 && (
-                    <hr className="hr-admin" />
-                  )}
-                </div>
-              );
-            })}
-            <Button variant="success" type="submit">
-              Iniciar
-            </Button>
-          </Form>
-        )}
-        <Button onClick={this.enviar}>Enviar</Button>
+        <Form onSubmit={e => this.update(e)}>
+          <Card.Title>Problemático #1</Card.Title>
+          <Form.Group>
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control
+              id="name1"
+              defaultValue={this.state.name1}
+              placeholder="Nombre de la problemática"
+              onChange={e => this.handleChange(e)}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Descripción</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows="4"
+              id="description1"
+              value={this.state.description1}
+              placeholder="Una pequeña descripción de la problemática"
+              onChange={e => this.handleChange(e)}
+            />
+          </Form.Group>
+          <Input
+            type="file"
+            id="iam"
+            className="attachInput-configure"
+            onChange={this.uploadButtonState}
+            disabled={this.state.filesSize === -1}
+          />
+          <hr className="hr-admin" />
+          <Button variant="success" type="submit">
+            Iniciar
+          </Button>
+        </Form>
+        <br />
+        <br />
+        <Button onClick={this.enviar}>State</Button>
       </Container>
     );
   }
