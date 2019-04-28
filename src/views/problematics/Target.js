c0369e3627d6347a5bcf5d4be29656993e27f600
Jerson Pérez <jersonantonio1998@gmail.com>
2019-04-28 12:50:29 -0500

    Comments ready! almost actions

--- src/views/problematics/Target.js
+++ src/views/problematics/Target.js
@@ -18,50 +18,107 @@ import QuestionAnswerOutlined from '@material-ui/icons/QuestionAnswerOutlined';
 
 class DetailedExpansionPanel extends React.Component {
     state = {
-        like: true,
+        like: false,
+        numLike : Number,
+        userName : false,
+        dislike: true,
+        numDislike : Number,
         info: this.props.info,
+        comments : false,
         body: "",
-        currentyId : this.props.currentyId
+        proposalId : this.props.info.id,
+        currentyId: this.props.currentyId
+    }
+    componentWillMount(){
+        const userName = localStorage.getItem("userId");
+        this.setState({
+            userName,
+            numLike : this.props.info.actions.like,
+            numDislike : this.props.info.actions.dislike,
+        })
+        console.log("info: ",this.props.info.actions.like)
+        if(this.props.info.comments){
+            const comments = this.props.info.comments;
+            const datos = [{}];
+            var i = 0;
+            for (var key in comments) {
+                for (var key1 in comments[key]) {
+                    datos[i] = {name:key1, date: key, body:comments[key][key1] }
+                }
+                i += 1;
+            }
+            this.setState({
+                comments: datos
+            });
+            console.log("datos:  ", datos)
+        }
     }
     update = () => {
         var database = firebase.database().ref();
         const date = new Date().getTime();
         const { body } = this.state;
-        const userId = localStorage.getItem("userId");
-        const tempObject = { [date]: { body, userId } };
+        const name = localStorage.getItem("userName");
+        const tempObject = {[date]:{[name]:body}};
+        const actions = {like:this.state.like};
+        console.log("database: ",this.state.proposalId)
         database
             .child("proposals")
             .child(this.state.currentyId)
+            .child(this.state.proposalId)
+            .child("comments")
             .update(tempObject)
+        database
+            .child("proposals")
+            .child(this.state.currentyId)
+            .child(this.state.proposalId)
+            .child("actions")
+            .update(actions)
+
+        this.state.comments.push({name:name, date:date, body:body})
+        this.setState({});
     };
-    onClick(){
-        console.log("onclick")
+    onClick(k) {
+        if(this.state.userName){
+            this.update();
+        }else{
+            window.location.pathname = "/login";
+        }
+    }
+    onClickActions(k){
+        if(this.state.userName){
+            if(k){
+                if(this.state.like !== this.state.dislike){
+                    this.setState({ 
+                        like: this.state.dislike,
+                    })
+                } 
+                this.setState({ 
+                    dislike: !this.state.dislike 
+                })                                  
+            }else{
+                if(this.state.dislike !== this.state.like ){
+                    this.setState({ 
+                        dislike: this.state.like ,
+                    })
+                }
+                this.setState({ 
+                    like: !this.state.like,
+                }
+            )}            
+        }else{
+            window.location.pathname = "/login";
+        }
     }
 
     render() {
-        console.log("render", this.state.info)
-        var comments = this.state.info.comments.map((dato, i) => {
-            return (
-                <Row key={i} className="row-comments">
-                    <Col>
-                        {dato.user}
-                    </Col>
-                    <Col className="classes-column classes-helper">
-                        <Typography variant="caption">
-                            {dato.body}
-                        </Typography>
-                    </Col>
-                </Row>
-            )
-        })
         return (
             <div className="classes-root">
-                <ExpansionPanel defaultExpanded>
+                <ExpansionPanel>
                     <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
                         <Container>
                             <Row>
                                 <Col xs={6} md={4}>
-                                    <Image width="180px" src={this.state.info.image ? this.state.info.image : "https://ep01.epimg.net/internacional/imagenes/2018/07/23/billete_a_macondo/1532310440_143390_1532310884_noticia_normal_recorte1.jpg"} rounded />
+                                    <Image width="180px" src={this.state.info.url ? this.state.info.url : "https://ep01.epimg.net/internacional/imagenes/2018/07/23/billete_a_macondo/1532310440_143390_1532310884_noticia_normal_recorte1.jpg"} rounded />
                                 </Col>
                                 <Col>
                                     <Row>
@@ -72,30 +129,51 @@ class DetailedExpansionPanel extends React.Component {
                                     <Row>
                                         {this.state.info.body}
                                     </Row>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col>
-                                    <Button size="small" color="primary">
-                                        <QuestionAnswerOutlined />
-                                    </Button>
-                                    <Button size="small" color="primary">
-                                        {this.state.like ? <ThumbUpOutlined /> : <ThumbUp />}
-                                    </Button>
-                                    <Button size="small" color="primary" onClick={() => { this.setState({ like: !this.state.like }) }}>
-                                        {this.state.like ? (
-                                            <ThumbDownOutlined />
-                                        ) : (
-                                            <ThumbDown />
-                                        )}
-                                    </Button>
+                                    <Row>
+                                        <Col></Col>
+                                        <Col></Col>
+                                        <Col>
+                                            <QuestionAnswerOutlined />                                        
+                                        </Col><Col>
+                                            {this.state.numDislike} {this.state.dislike ? <ThumbUpOutlined /> : <ThumbUp />}
+                                        </Col><Col>
+                                            {this.state.numLike}{this.state.like ? <ThumbDownOutlined /> : <ThumbDown />}                                            
+                                        </Col>
+                                    </Row>
                                 </Col>
                             </Row>
                         </Container>
                     </ExpansionPanelSummary>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <Button size="small" color="primary" onClick={() => { this.onClickActions(true) }}>
+                                    {this.state.numDislike}{this.state.dislike ? <ThumbUpOutlined /> : <ThumbUp />}
+                                </Button>
+                                <Button size="small" color="primary" onClick={() => { this.onClickActions(false) }}>
+                                    {this.state.numLike}{this.state.like ? <ThumbDownOutlined /> : <ThumbDown />}
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Container>
                     <ExpansionPanelDetails className="classes-details">
                         <Container>
-                            {comments}
+                            {this.state.comments ?
+                                this.state.comments.map((dato, i) => {
+                                    return (
+                                        <Row key={i} className="row-comments">
+                                            <Divider />
+                                            <Col>
+                                                {dato.name}
+                                            </Col>
+                                            <Col className="classes-column classes-helper">
+                                                <Typography variant="caption">
+                                                    {dato.body}
+                                                </Typography>
+                                            </Col>
+                                        </Row>
+                                    )
+                                }) : null}
                             <Row className="row-comments">
                                 <Form.Label>Comentar</Form.Label>
                                 <Form.Control
@@ -108,13 +186,13 @@ class DetailedExpansionPanel extends React.Component {
                             </Row>
                         </Container>
                     </ExpansionPanelDetails>
-                    <Divider />
                     <ExpansionPanelActions>
-                        <Button size="small" color="primary" onClick={this.onClick()} >
+                        <Button size="small" color="primary" onClick={() => this.onClick(true)} >
                             Guardar
                         </Button>
                     </ExpansionPanelActions>
                 </ExpansionPanel>
+                <Divider />
             </div>
         );
     }
--- src/views/problematics/index.js
+++ src/views/problematics/index.js
@@ -3,85 +3,78 @@ import { Container, Col, Row } from "react-bootstrap";
 import Target from "./Target";
 import * as firebase from "firebase";
 import Button from '@material-ui/core/Button';
+import PlayCircleOutlined from '@material-ui/icons/PlayCircleOutline';
+
 
 class Problematics extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      currentyId: Number,
-      data: []
-    };
-  }
-  componentWillMount() {
-    var database = firebase.database().ref();
-    const currentyId = this.props.match.params.id;
-    database
-      .child("problems")
-      .once("value")
-      .then(data => {
-        const dbinfo = data.toJSON();
-        this.setState({ name: dbinfo["name" + currentyId] });
-      });
+        this.state = {
+            currentyId: Number,
+            data: [],
+            key : null
+        };
+    }
+    componentWillMount() {
+        var database = firebase.database().ref();
+        const currentyId = this.props.match.params.id;
+        database
+            .child("proposals")
+            .child(currentyId.toString())
+            .once("value")
+            .then(data => {
+                var datos = [];
+                const dbinfo = data.toJSON();
+                data.forEach(value => {
+                    datos.push(value.val());
+                });
+                var i = 0;
+                for (var key in dbinfo) {
+                    datos[i].id = key;
+                    i = +1;
+                }
+                this.setState({
+                    data: datos,
+                    currentyId
+                });
+            });
+    }
 
-    this.setState({
-      currentyId: this.props.match.params.id
-    });
-
-    database
-      .child("proposals")
-      .child(this.props.match.params.id.toString())
-      .once("value")
-      .then(data => {
-        var datos = [];
-        const dbinfo = data.toJSON();
-        data.forEach(value => {
-          datos.push(value.val());
+    render() {
+        var card = this.state.data.map((dato, i) => {
+            return (
+                <div key={i}>
+                    <Target info={dato} currentyId={this.state.currentyId}/>
+                </div>
+            );
         });
-        var i = 0;
-        for (var key in dbinfo) {
-          datos[i].id = key;
-          i = +1;
-        }
-        this.setState({
-          data: datos
-        });
-        console.log("s: ",this.state.data)
-      });
-  }
-
-  render() {
-      console.log("info: ",this.state.data)
-    var card = this.state.data.map((dato, i) => {
-      return (
-        <div key={i}>
-          <Target info={dato} currentyId={this.state.currentyId}/>
-        </div>
-      );
-    });
-    return (
-      <div>
-        <Container>
-          <Row>
-            <h1>Propuestas para mejorar la {this.state.name}</h1>
-            <Button
-                  color="danger"
-                  size="lg"
-                  href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="fas fa-play" />
-                  Watch video
-            </Button>
-          </Row>
-          <Row>
-            <Col>{card}</Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+        return (
+            <div>
+                <Container>
+                    <Row>
+                        <h1>Propuestas para mejorar la {this.state.name}</h1>
+                    </Row>
+                    <Row>
+                        <Button
+                            color="danger"
+                            size="lg"
+                            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <PlayCircleOutlined />
+                            Watch video
+                        </Button>
+                    </Row>
+                    <Row></Row>
+                    <Row>
+                        <Col>{card}</Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default Problematics;
